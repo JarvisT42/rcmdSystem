753dd683c970a13bc45b4408b9fda3be83f1a515
joshua <kentjoshuazamoradaborbor@gmail.com>
2025-12-12 15:22:41 +0800

    first commit

---
+++ client/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+
+
+   <div className="font-sans grid place-items-center min-h-screen p-8 bg-red-200">
+  <p className="text-gray-600 text-sm">
+    This is a clean Tailwind-styled box component.
+  </p>
+</div>
+
+
+  );
+}
