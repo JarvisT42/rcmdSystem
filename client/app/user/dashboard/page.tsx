2f987db6372e8466d6aa076b155e3ac7441555c3
kent joshua <kentjoshuazamoradaborbor@gmail.com>
2025-12-13 19:37:35 +0800

    12/13/2025-7:37PM

--- client/app/user/dashboard/page.tsx
+++ client/app/user/dashboard/page.tsx
@@ -1,9 +1,200 @@
+"use client";
+
+import * as React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+type ItemRow = {
+  qty: string;
+  unit: string;
+  item: string;
+  description: string;
+};
+
+const people = ["Juan Dela Cruz", "Maria Santos", "Pedro Reyes", "Ana Lopez"];
+
 export default function DashboardPage() {
+  const [items, setItems] = React.useState<ItemRow[]>([
+    { qty: "", unit: "", item: "", description: "" },
+  ]);
+
+  const handleChange = (index: number, field: keyof ItemRow, value: string) => {
+    const updated = [...items];
+    updated[index][field] = value;
+
+    // auto-add new row if typing in last row
+    if (
+      index === items.length - 1 &&
+      Object.values(updated[index]).some((v) => v !== "")
+    ) {
+      updated.push({ qty: "", unit: "", item: "", description: "" });
+    }
+
+    setItems(updated);
+  };
+
   return (
-    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-      <div className="bg-gray-200 aspect-video rounded-xl" />
-      <div className="bg-gray-200 aspect-video rounded-xl" />
-      <div className="bg-gray-200 aspect-video rounded-xl" />
+    <div className="grid gap-4 h-full">
+      <div className="rounded-xl border shadow-lg p-6 bg-white">
+        <h2 className="text-xl font-semibold mb-4">Recommendation Form</h2>
+
+        <form className="grid gap-6">
+          {/* DETAILS */}
+          {/* DETAILS + SERIES NO */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="flex flex-col gap-2">
+              <Label className="mt-1">Series No.</Label>
+              <Input placeholder="SR-0001" />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label className="mt-1">Details</Label>
+              <Textarea
+                placeholder="Enter details..."
+                rows={3}
+                defaultValue="We would like to request"
+              />
+            </div>
+          </div>
+
+          {/* DATE & PO */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="grid gap-2">
+              <Label>Date</Label>
+              <Input type="date" />
+            </div>
+            <div className="grid gap-2">
+              <Label>PO Number</Label>
+              <Input placeholder="PO-0001" />
+            </div>
+          </div>
+
+          {/* REQUESTED ITEMS TABLE */}
+          <div className="grid gap-2">
+            <Label>Requested Items</Label>
+
+            <div className="overflow-x-auto rounded-xl border">
+              <table className="w-full border-collapse">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-2 py-1 text-left w-24 rounded-tl-xl">
+                      Qty
+                    </th>
+                    <th className="px-2 py-1 text-left w-28">Unit</th>
+                    <th className="px-2 py-1 text-left">Item</th>
+                    <th className="px-2 py-1 text-left rounded-tr-xl">
+                      Description
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {items.map((row, index) => (
+                    <tr key={index}>
+                      <td
+                        className={`top p-1 ${
+                          index === items.length - 1 ? "rounded-bl-xl" : ""
+                        }`}
+                      >
+                        <Input
+                          value={row.qty}
+                          onChange={(e) =>
+                            handleChange(index, "qty", e.target.value)
+                          }
+                        />
+                      </td>
+                      <td
+                        className={`p-1 ${
+                          index === items.length - 1 ? "" : ""
+                        }`}
+                      >
+                        <Input
+                          value={row.unit}
+                          onChange={(e) =>
+                            handleChange(index, "unit", e.target.value)
+                          }
+                        />
+                      </td>
+                      <td
+                        className={`p-1 ${
+                          index === items.length - 1 ? "" : ""
+                        }`}
+                      >
+                        <Input
+                          value={row.item}
+                          onChange={(e) =>
+                            handleChange(index, "item", e.target.value)
+                          }
+                        />
+                      </td>
+                      <td
+                        className={`p-1 ${
+                          index === items.length - 1 ? "rounded-br-xl" : ""
+                        }`}
+                      >
+                        <Input
+                          value={row.description}
+                          onChange={(e) =>
+                            handleChange(index, "description", e.target.value)
+                          }
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          {/* SIGNATORIES */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <SelectField label="Requested By" />
+            <SelectField label="Checked By" />
+
+            <SelectField label="Received By" />
+            <SelectField label="Management" />
+
+            <SelectField label="Approved By" />
+            <SelectField label="Head" />
+          </div>
+
+          {/* SUBMIT */}
+          <div className="flex justify-start gap-2 ">
+            <Button type="submit">Save</Button>
+            <Button type="submit">Update</Button>
+            <Button type="submit">Print</Button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
+}
+
+/* Reusable Select */
+function SelectField({ label }: { label: string }) {
+  return (
+    <div className="grid gap-2">
+      <Label>{label}</Label>
+      <Select>
+        <SelectTrigger>
+          <SelectValue placeholder={`Select ${label}`} />
+        </SelectTrigger>
+        <SelectContent>
+          {people.map((person) => (
+            <SelectItem key={person} value={person}>
+              {person}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
 }
--- client/app/user/history/page.tsx
+++ client/app/user/history/page.tsx
@@ -1,8 +1,339 @@
-export default function HistoryPage() {
+"use client";
+
+import * as React from "react";
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+  type ColumnDef,
+  type ColumnFiltersState,
+  type SortingState,
+  type VisibilityState,
+} from "@tanstack/react-table";
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const data: Payment[] = [
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@example.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@example.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@example.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@example.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@example.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@example.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@example.com",
+  },
+  {
+    id: "af",
+    amount: 721,
+    status: "failed",
+    email: "carmella@example.com",
+  },
+];
+
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  // In your columns definition, update the serial column header:
+  {
+    id: "serial",
+    header: () => <div style={{ whiteSpace: "pre" }}>Series No.</div>,
+    cell: ({ row }) => <div>{row.index + 1}</div>,
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status") as string;
+      const statusColor =
+        {
+          success: "bg-green-100 text-green-800",
+          processing: "bg-blue-100 text-blue-800",
+          pending: "bg-yellow-100 text-yellow-800",
+          failed: "bg-red-100 text-red-800",
+        }[status] || "bg-gray-100 text-gray-800";
+
+      return (
+        <div className="flex items-center">
+          <span
+            className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${statusColor}`}
+          >
+            {status}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+
+      // Format the amount as a dollar amount
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
+export default function DataTableDemo() {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+    },
+  });
+
   return (
-    <div className="bg-white p-6 rounded-xl shadow-sm">
-      <h1 className="text-2xl font-bold mb-4">History</h1>
-      <p>Your history content goes here...</p>
+    <div className="grid gap-4 h-full">
+      <div className="rounded-xl border shadow-lg p-6 bg-white">
+        <h2 className="text-xl font-semibold mb-4">List</h2>
+
+        <div className="w-full">
+          <div className="flex items-center py-4">
+            <Input
+              placeholder="Filter emails..."
+              value={
+                (table.getColumn("email")?.getFilterValue() as string) ?? ""
+              }
+              onChange={(event) =>
+                table.getColumn("email")?.setFilterValue(event.target.value)
+              }
+              className="max-w-sm"
+            />
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="ml-auto">
+                  Columns <ChevronDown className="ml-2 h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                {table
+                  .getAllColumns()
+                  .filter(
+                    (column) =>
+                      column.getCanHide() &&
+                      column.id !== "serial" &&
+                      column.id !== "actions"
+                  )
+                  .map((column) => {
+                    return (
+                      <DropdownMenuCheckboxItem
+                        key={column.id}
+                        className="capitalize"
+                        checked={column.getIsVisible()}
+                        onCheckedChange={(value) =>
+                          column.toggleVisibility(!!value)
+                        }
+                      >
+                        {column.id === "amount" ? "Amount" : column.id}
+                      </DropdownMenuCheckboxItem>
+                    );
+                  })}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <div className="overflow-hidden rounded-md border">
+            <Table>
+              <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                      return (
+                        <TableHead key={header.id}>
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                              )}
+                        </TableHead>
+                      );
+                    })}
+                  </TableRow>
+                ))}
+              </TableHeader>
+              <TableBody>
+                {table.getRowModel().rows?.length ? (
+                  table.getRowModel().rows.map((row) => (
+                    <TableRow key={row.id}>
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell
+                      colSpan={columns.length}
+                      className="h-24 text-center"
+                    >
+                      No results.
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </div>
+          <div className="flex items-center justify-end space-x-2 py-4">
+            <div className="text-muted-foreground flex-1 text-sm">
+              Showing {table.getRowModel().rows.length} of {data.length} entries
+            </div>
+            <div className="space-x-2">
+              <Button
+                variant="outline"
+                size="sm"
+                onClick={() => table.previousPage()}
+                disabled={!table.getCanPreviousPage()}
+              >
+                Previous
+              </Button>
+              <Button
+                variant="outline"
+                size="sm"
+                onClick={() => table.nextPage()}
+                disabled={!table.getCanNextPage()}
+              >
+                Next
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
